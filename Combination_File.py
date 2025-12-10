6a2cccc2acfcfbfee4a7e12f5cec079ba3881e94
Cassandra Doll <107585623+Cass-Doll@users.noreply.github.com>
2025-12-10 18:59:24 -0500

    Update Combination_File.py

--- Combination_File.py
+++ Combination_File.py
@@ -1,25 +1,26 @@
 import pandas as pd
 
-# Paths to the CSV files
+#path to csv files
 csv1 = "Flip_and_Remove.csv"
-csv2 = "filtered_output.csv"
+csv2 = "final_output.csv"
 csv3 = "Wisconsin_Only.csv"
 
-# Read the CSVs normally (keep headers)
+#read csvs
 df1 = pd.read_csv(csv1)
 df2 = pd.read_csv(csv2)
 df3 = pd.read_csv(csv3)
 
-# Reset indices to ensure they align from the top row
+#start from the top row when combining the files
 df1.reset_index(drop=True, inplace=True)
 df2.reset_index(drop=True, inplace=True)
 df3.reset_index(drop=True, inplace=True)
 
-# Combine side-by-side
+#combine side by side
 combined_df = pd.concat([df1, df2, df3], axis=1)
 
-# Save result
+#save the result
 output_file = "combined.csv"
 combined_df.to_csv(output_file, index=False)
+
 
 print(f"Combined CSV saved as {output_file}")
