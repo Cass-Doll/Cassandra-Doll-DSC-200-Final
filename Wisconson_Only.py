dccafe65d017cca0cca1b7b4d6ae6d8f3f2e8ff2
Cassandra Doll <107585623+Cass-Doll@users.noreply.github.com>
2025-12-10 19:02:25 -0500

    Update Wisconson_Only.py

--- Wisconson_Only.py
+++ Wisconson_Only.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
 
-input_file = "data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"     # your input
-output_file = "Wisconsin_Only.csv"  # cleaned output
+input_file = "data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv"
+output_file = "Wisconsin_Only.csv"
 
-# Load dataset
+#dataset
 df = pd.read_csv(input_file)
 
-# Keep only rows where StateName == "WI"
+#only rows where StateName is WI
 df_WI = df[df["StateName"] == "WI"]
 
-# Save filtered result
+#save result
 df_WI.to_csv(output_file, index=False)
 
 print(f"Saved WI-only dataset as '{output_file}'.")
 print("Original rows:", len(df))
 print("Rows with StateName == 'WI':", len(df_WI))
+
