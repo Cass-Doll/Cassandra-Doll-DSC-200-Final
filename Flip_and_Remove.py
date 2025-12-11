68f13c795f06e1e2226a3f206eacb1abde12adab
Cassandra Doll <107585623+Cass-Doll@users.noreply.github.com>
2025-12-10 19:01:10 -0500

    Update Flip_and_Remove.py

--- Flip_and_Remove.py
+++ Flip_and_Remove.py
@@ -1,23 +1,22 @@
 import pandas as pd
 
-# Read the CSV file
-input_file = "data/ACSDP1Y2024.DP05-Data.csv"  # replace with your CSV file path
+#read the csv
+input_file = "data/ACSDP1Y2024.DP05-Data.csv"
 df = pd.read_csv(input_file)
 
-# Transpose the DataFrame
+#transpose the dataframe
 df_transposed = df.T
-
-# Reset the index to make it easier to manipulate rows
 df_transposed.reset_index(inplace=True, drop=True)
 
-# Remove rows containing "Margin of Error!!" anywhere
+#remove rows with "Margin of Error!!"
 df_transposed = df_transposed[~df_transposed.apply(lambda row: row.astype(str).str.contains("Margin of Error!!").any(), axis=1)]
 
-# Remove the last row
+#remove the last row (empty)
 df_transposed = df_transposed.iloc[:-1]
 
-# Save the result to a new CSV
-output_file = "Flip_and_Remove.csv"  # Replace with desired output file path
+#save to csv
+output_file = "Flip_and_Remove.csv"
 df_transposed.to_csv(output_file, index=False)
+
 
 print(f"Processed CSV saved to {output_file}")
